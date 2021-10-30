0767a539b3c7ef636b42f52858a3ea6e9ede1f37
Ronald Mannak <ronaldmannak@me.com>
2021-10-30 13:24:11 -0700

    Add MEWwalletKit dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "SafariWalletCore",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,15 +19,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "MEWwalletKit", url: "https://github.com/MyEtherWallet/mew-wallet-ios-kit", .upToNextMajor(from: "1.4.13"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SafariWalletCore",
-            dependencies: []),
+            dependencies: ["MEWwalletKit"]),
         .testTarget(
             name: "SafariWalletCoreTests",
-            dependencies: ["SafariWalletCore"]),
+            dependencies: ["SafariWalletCore", "MEWwalletKit"]),
     ]
 )
